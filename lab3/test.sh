4204d40c6ca3935e17fe670f6972f34726a6bc80
Mikhail Ershov <43749705+ershovmn@users.noreply.github.com>
2020-11-06 12:59:57 +0300

    Update test.sh

--- lab3/test.sh
+++ lab3/test.sh
@@ -2,7 +2,7 @@
 type=$(cat /etc/shadow | grep $USER | awk -F: '{print $2}' | awk -F '\\$' '{print $2}')
 salt=$(cat /etc/shadow | grep $USER | awk -F: '{print $2}' | awk -F '\\$' '{print $3}')
 hash=$(cat /etc/shadow | grep $USER | awk -F: '{print $2}' | awk -F '\\$' '{print $4}')
-passwd = $(openssl passwd -$tpe -salt $salt -crypt $(head /dev/urandom | tr -dc A-Za-z0-9 | head -c 16))
+passwd = $(openssl passwd -$type -salt $salt -crypt $(head /dev/urandom | tr -dc A-Za-z0-9 | head -c 16))
 while [1 = 1]
   if [ "$passwd" = "$hash" ]; then
     passwd = $(openssl passwd -$tpe -salt $salt -crypt $(head /dev/urandom | tr -dc A-Za-z0-9 | head -c 16))
